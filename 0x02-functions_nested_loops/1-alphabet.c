504c046627933c724faf13e4f264a071b86623a1
BeloALX <123982100+BeloALX@users.noreply.github.com>
2023-02-21 22:44:59 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,12 @@
-#include “main.h”
+#include "main.h"
 
 /**
  * print_alphabet – Create alphabets
  * Return: void
  */
+
 void print_alphabet(void)
+	
 {
 	char c;
 	for (c = ‘a’;  c <= ‘z’; c++)
