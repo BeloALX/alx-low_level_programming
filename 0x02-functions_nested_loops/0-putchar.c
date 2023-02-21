6989736f3edecdc0fe21654856951359ad5f1228
Thabelo <thabelojennifer2@gmail.com>
2023-02-21 11:36:35 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+ * main - print _putchar
+ * Return: 0
+ */
 
+int main(void)
 
-	int main(void)
-	{
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -20,4 +19,4 @@
 	_putchar('\n');
 	return (0);
 
-	}
+}
