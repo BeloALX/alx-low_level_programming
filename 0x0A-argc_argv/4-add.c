a82f1f3dae556295b6e69855cba8dddd82bc9657
BeloALX <123982100+BeloALX@users.noreply.github.com>
2023-03-10 21:25:07 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,11 +8,12 @@
  *
  * Return: Always 0 (Success)
  */
- int check_num(char *str)
+int check_num(char *str)
 {
 	/*Declaring variables*/
 	unsigned int count;
 	count = 0;
+	
 	while (count < strlen(str)) /*count string*/
 		{
 			if (!isdigit(str[count])) /*check if str there are digit*/
@@ -36,6 +37,7 @@
 		int count;
 		int str_to_int;
 		int sum = 0;
+		
 		count = 1;
 		while (count < argc) /*Goes through the whole array*/
 		{
